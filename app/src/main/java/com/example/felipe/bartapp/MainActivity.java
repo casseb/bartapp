dec3065bf27e0919f68dd5d6c2ec0e617fe40973
casseb <felipe.casseb@gmail.com>
2017-03-27 22:12:37 -0300

    fazendo comunicação com web service

--- app/src/main/java/com/example/felipe/bartapp/MainActivity.java
+++ app/src/main/java/com/example/felipe/bartapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.felipe.bartapp;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void onClickEnter(View view){
+
+        EditText txtUser = (EditText) findViewById(R.id.editTextUsuario);
+        EditText txtPassword = (EditText) findViewById(R.id.editTextSenha);
+        TextView resultArea = (TextView) findViewById(R.id.Resultado);
+
+        String username = String.valueOf(txtUser.getText());
+        /*parei aqui kkkkkk*/
+
     }
 }
