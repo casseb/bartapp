16d3319bff902c744ad5e03cd4b1e28aeaab7aeb
By Casseb <felipe.casseb@gmail.com>
2017-06-18 01:56:57 -0300

    Update Connection.java

--- app/src/main/java/com/example/felipe/bartapp/Connection.java
+++ app/src/main/java/com/example/felipe/bartapp/Connection.java
@@ -4,57 +4,94 @@ package com.example.felipe.bartapp;
  * Created by Felipe on 27/03/2017.
  */
 
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Connection{
 
     private final String USER_AGENT = "Mozilla/5.0";
 
 
+
     // HTTP GET request
-    public String login(String userName,String password) throws Exception {
+    public String login(String userName,String password) throws Exception{
 
-        //https://api.myjson.com/bins/3kpyw
-        //http://api.flickr.com/services/feeds/photos_public.gne?tags=beatles&format=json&jsoncallback=?
-        String url = "https://lisawebservicetest.herokuapp.com/login/"+userName+"/"+password;
+        String urlString = "https://lisawebservicetest.herokuapp.com/login";
 
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        JSONObject postDataParams = new JSONObject();
+        postDataParams.put("usuario", userName);
+        postDataParams.put("senha", password);
+        Log.e("params",postDataParams.toString());
 
-        // optional default is GET
-        con.setRequestMethod("GET");
+        return request(urlString,postDataParams);
 
-        //add request header
-        con.setRequestProperty("User-Agent", USER_AGENT);
+    }
 
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
+    public String request(String urlString, JSONObject postDataParams) throws Exception{
 
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        URL url = new URL(urlString);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(15000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(os, "UTF-8"));
+        writer.write(postDataParams.toString());
+
+        writer.flush();
+        writer.close();
+        os.close();
+
+        int responseCode=conn.getResponseCode();
+
+        if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+            BufferedReader in=new BufferedReader(
+                    new InputStreamReader(
+                            conn.getInputStream()));
+            StringBuffer sb = new StringBuffer("");
+            String line="";
+
+            while((line = in.readLine()) != null) {
+
+                sb.append(line);
+                break;
+            }
+
+            in.close();
+            return sb.toString();
+
         }
-        in.close();
-
-        //System.out.println(response.toString());
-
-        String found = response.toString();
-
-        return found;
+        else {
+            return new String("false : "+responseCode);
+        }
     }
 
     /*
